a941bd12e9eca40f0c44470fae230fb0114aca8a
Igor Gubin <igor.gubin@privatbank.ua>
2021-10-29 21:50:49 +0300

    Init commit

--- src/com/pb/gubin/hw3/Array.java
+++ src/com/pb/gubin/hw3/Array.java
@@ -1,4 +1,21 @@
 package com.pb.gubin.hw3;
 
-public class Array {
+import java.util.Scanner;
+
+public class Array<box> {
+    public static void main(String[] args) {
+    int[] box = new int[10]; // Определяем длину массива
+    String[] boxes = new String[10];
+    Scanner input = new Scanner(System.in); // Объявляем Scanner
+        System.out.println("Insert array elements:");
+        /*Пройдёмся по всему массиву, заполняя его*/
+        for (int i = 0; i < 10; i++) {
+            box[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+        }
+        System.out.print ("Inserted array elements:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print (" " + box[i]); // Выводим на экран, полученный массив
+        }
+        System.out.println();
+}
 }
---
+++ src/com/pb/gubin/hw3/Test2.java
@@ -0,0 +1,9 @@
+package com.pb.gubin.hw3;
+
+public class Test2 {
+    public static void main(String[] args) {
+    int[] myArray; // объявление массива
+    myArray = new int[10]; // создание, то есть, выделение памяти для массива на 10 элементов типа int
+        System.out.println(myArray.length); // вывели в консоль длину массива, то есть количество элементов, которые мы можем поместить в массив
+}
+    }
